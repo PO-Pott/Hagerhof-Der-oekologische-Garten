7158841511a69bb4b3e46953f56fef013522fe85
PO-Pott <spesmeus03@t-online.de>
2026-04-29 17:15:09 +0200

    Add function to display message in output element

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log("Hallo von JavaScript!");
+function zeigeNachricht() {
+    document.getElementById("ausgabe").textContent =
+        "Der Makerspace ist eine Werkstatt des Hagerhof in der man IT,KI und handwerkliche Projekt umsestzen kann ";
+}
