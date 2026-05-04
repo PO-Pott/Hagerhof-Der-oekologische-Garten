fa18dae9b42e5fd32a14d645ab835c796c56c322
PO-Pott <spesmeus03@t-online.de>
2026-05-04 19:51:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 function zeigeNachricht() {
     document.getElementById("ausgabe").textContent =
-        "Der Makerspace ist eine Werkstatt des Hagerhof in der man IT,KI und handwerkliche Projekt umsestzen kann ";
+        "Der Makerspace ist eine Werkstatt, in der Schülerinnen und Schüler der Privatschule Hagerhof (Bad Honnef) IT,3d-Druck und handwerkliche Projekt umsetzen können";
 }
